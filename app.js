efa4ba15ab4c8dae9715fe7089657ad8aa733f95
Neeraj Kumar <nrjkumar1@outlook.com>
2020-09-07 05:16:32 +0530

    Final project with local DB

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-let blogs = [];
 
 const app = express();
 
@@ -17,38 +16,81 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res){
-  res.render("home", {start: homeStartingContent, posts: blogs});
+  Post.find({}, function(err, posts){
+    res.render("home", {start: homeStartingContent,posts: posts});
+  });
 });
 
 app.get("/about", function(req, res){
-  res.render("aboutcontact", {title: "About", content: aboutContent});
+  Post.find({}, function(err, posts){
+    res.render("aboutcontact", {title: "About", content: aboutContent, posts: posts});
+  });
 });
 
 app.get("/contact", function(req, res){
-  res.render("aboutcontact", {title: "Contact", content: contactContent});
+  Post.find({}, function(err, posts){
+    res.render("aboutcontact", {title: "Contact", content: contactContent, posts: posts});
+  });
 });
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+  Post.find({}, function(err, posts){
+    res.render("compose", {posts: posts});
+ });
 });
 
 app.get("/posts/:postName", function(req, res){
   let requestedTitle = lodash.lowerCase(req.params.postName);
-  blogs.forEach(function(blog){
-    let storedTitle = lodash.lowerCase(blog.newTitle);
-    if(storedTitle === requestedTitle){
-      res.render("post", {title: blog.newTitle, post: blog.newBody});
-    };
+  Post.find({}, function(err, posts){
+    posts.forEach(function(post){
+      let storedTitle = lodash.lowerCase(post.title);
+      if(storedTitle === requestedTitle){
+        res.render("post", {title: post.title, content: post.content, posts: posts});
+      };
+    });
   });
 });
 
 app.post("/compose", function(req, res){
-  let content = {
-    newTitle: req.body.title,
-    newBody: req.body.body
-  };
-  blogs.push(content);
+
+  // Post.find({}, function(err, posts){
+  //   posts.forEach(function(post){
+  //     if(post.title === req.body.title){
+  //       res.redirect("/about");
+  //     } else {
+  //       const post = new Post({
+  //         title: req.body.title,
+  //         content: req.body.body
+  //       });
+  //
+  //       post.save();
+  //     };
+  //   });
+  //
+  // });
+
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.body
+  });
+
+  post.save();
+
+  // let content = {
+  //   newTitle: req.body.title,
+  //   newBody: req.body.body
+  // };
+  // blogs.push(content);
   res.redirect("/");
 });
 
