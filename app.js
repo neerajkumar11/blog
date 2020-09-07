265749362a20d29952093a51e1d2615b568d75a1
Neeraj Kumar <nrjkumar1@outlook.com>
2020-09-07 05:42:46 +0530

    Add remote DB

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-neeraj:toughPassword@blogcluster.djzf5.gcp.mongodb.net/blogDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
@@ -99,6 +99,11 @@ app.post("/search", function(req, res){
   res.redirect("posts/:"+searchTitle);
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+};
+
+app.listen(port, function(){
+  console.log("Server Running");
 });
